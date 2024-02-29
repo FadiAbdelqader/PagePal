eb2e70993946c8e3a5f88cde88ca6e3cf56c2cb4
FadiAbdelqader <abdelqader.fadi.h@gmail.com>
2024-02-29 12:05:32 +0100

    init

---
+++ src/main/java/com/example/PagePal/PagePalApplication.java
@@ -0,0 +1,13 @@
+package com.example.PagePal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagePalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagePalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PagePal/PagePalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PagePal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagePalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
